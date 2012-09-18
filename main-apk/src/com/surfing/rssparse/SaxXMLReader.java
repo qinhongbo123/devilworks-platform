0dfb80ddce1918d97539e7a127680b1873fbed1d
leo.coldblood <leo.coldblood@gmail.com>
2012-09-18 23:02:08 +0800

    remove non utf-8 characters in project

--- main-apk/src/com/surfing/rssparse/SaxXMLReader.java
+++ main-apk/src/com/surfing/rssparse/SaxXMLReader.java
@@ -15,33 +15,43 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import android.content.Context;
 
-public class SaxXMLReader {
+public class SaxXMLReader
+{
 	private static final String TAG = "SaxXMLReader";
 	private ChannelItem channelitem = null;
 	private String mCurrentItem = null;
-	public  ChannelInformation readXML(Context context, InputStream instream) {
-		
-		SAXParserFactory factory=SAXParserFactory.newInstance();
-		try{
-			 SAXParser parser=factory.newSAXParser();
-			//获取事件源
-	         XMLReader xmlReader=parser.getXMLReader();
-	        //设置处理器
-            XmlHandle handler=new XmlHandle();
-            xmlReader.setContentHandler(handler);
-            xmlReader.parse(new InputSource(instream));
-            instream.close();
-            return handler.getChannleinfo();
-		}catch(ParserConfigurationException e){
+
+	public ChannelInformation readXML(Context context, InputStream instream)
+	{
+
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		try
+		{
+			SAXParser parser = factory.newSAXParser();
+			XMLReader xmlReader = parser.getXMLReader();
+			XmlHandle handler = new XmlHandle();
+			xmlReader.setContentHandler(handler);
+			xmlReader.parse(new InputSource(instream));
+			instream.close();
+			return handler.getChannleinfo();
+		}
+		catch (ParserConfigurationException e)
+		{
 			e.printStackTrace();
-		}catch(SAXException e){
+		}
+		catch (SAXException e)
+		{
 			e.printStackTrace();
-		}catch(IOException e){
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 		return null;
 	}
-	public  class XmlHandle extends DefaultHandler{
+
+	public class XmlHandle extends DefaultHandler
+	{
 		private ChannelInformation channleinfo = null;
 		private String mChannelTag = "item";
 		private String mTitleTag = "title";
@@ -49,52 +59,67 @@ public class SaxXMLReader {
 		private String mDescTag = "description";
 		private String mIconTag = "icon";
 		private String mDateTag = "pubDate";
-		
-		public XmlHandle(){
+
+		public XmlHandle()
+		{
 			channleinfo = new ChannelInformation();
 		}
-		public ChannelInformation getChannleinfo() {
+
+		public ChannelInformation getChannleinfo()
+		{
 			return channleinfo;
 		}
 
 		@Override
-		public void endElement(String uri, String localName, String qName)
-				throws SAXException {
-			if(mChannelTag.equals(localName)){
+		public void endElement(String uri, String localName, String qName) throws SAXException
+		{
+			if (mChannelTag.equals(localName))
+			{
 				channleinfo.addChannelItem(channelitem);
 				channelitem = null;
 			}
 		}
 
 		@Override
-		public void startDocument() throws SAXException {
-			
+		public void startDocument() throws SAXException
+		{
+
 		}
 
 		@Override
-		public void startElement(String uri, String localName, String qName,
-				Attributes attributes) throws SAXException {
+		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException
+		{
 			mCurrentItem = localName;
-			if(mChannelTag.equals(localName)){
+			if (mChannelTag.equals(localName))
+			{
 				channelitem = new ChannelItem();
 			}
 		}
 
 		@Override
-		public void characters(char[] ch, int start, int length)
-				throws SAXException {
-			if(mTitleTag.equals(mCurrentItem)){
-				channelitem.setmTitle(new String(ch,start,length));
-			}else if(mLinkTag.equals(mCurrentItem)){
-				channelitem.setmLink(new String(ch,start,length));
-			}else if(mDescTag.equals(mCurrentItem)){
-				channelitem.setmDescription(new String(ch,start,length));
-			}else if(mIconTag.equals(mCurrentItem)){
-				channelitem.setmIcon(new String(ch,start,length));
-			}else if(mDateTag.equals(mCurrentItem)){
-				channelitem.setmDate(new String(ch,start,length));
+		public void characters(char[] ch, int start, int length) throws SAXException
+		{
+			if (mTitleTag.equals(mCurrentItem))
+			{
+				channelitem.setmTitle(new String(ch, start, length));
+			}
+			else if (mLinkTag.equals(mCurrentItem))
+			{
+				channelitem.setmLink(new String(ch, start, length));
+			}
+			else if (mDescTag.equals(mCurrentItem))
+			{
+				channelitem.setmDescription(new String(ch, start, length));
+			}
+			else if (mIconTag.equals(mCurrentItem))
+			{
+				channelitem.setmIcon(new String(ch, start, length));
+			}
+			else if (mDateTag.equals(mCurrentItem))
+			{
+				channelitem.setmDate(new String(ch, start, length));
 			}
 		}
-		
+
 	}
 }
