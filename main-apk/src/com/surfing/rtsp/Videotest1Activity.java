f5d8b572b7e725f1b1f7e151f631f29a6ebcb1c8
ragentek <chenmeimei_8899@163.com>
2012-12-08 23:47:31 +0800

    build error

--- main-apk/src/com/surfing/rtsp/Videotest1Activity.java
+++ main-apk/src/com/surfing/rtsp/Videotest1Activity.java
@@ -29,7 +29,7 @@ public class Videotest1Activity extends ActivityBase {
     VideoView videoView1;
     ListView mVideoList = null;
     SimpleAdapter mVideoAdapter = null;
-    ArrayList<HashMap<String, Object>> mVideoArray = new ArrayList<HashMap<String, Object>>();
+    ArrayList<HashMap<String,Object>> mVideoArray = new ArrayList<HashMap<String,Object>>();
     Context mContext = null;
     ProgressDialog  mWaitDialog = null;
     String rtspUrlZhonglu   = "rtsp://117.35.58.70:2554/service?PuId-ChannelNo=27000000000000000011200027400000-1&PlayMethod=0&StreamingType=2";
@@ -44,11 +44,10 @@ public class Videotest1Activity extends ActivityBase {
         mContext = this.getApplicationContext();
         mVideoList = (ListView)findViewById(R.id.video_list);
         videoView1 = (VideoView)findViewById(R.id.videoView1);
-
+        
         //init data
         getVideoList();
-
-        mVideoAdapter = new SimpleAdapter(mContext, mVideoArray,
+        mVideoAdapter = new SimpleAdapter(mContext,mVideoArray,
                 R.layout.videolist_item_layout,
                 new String[]{"road"},
                 new int[]{R.id.video_item_text});
@@ -60,8 +59,8 @@ public class Videotest1Activity extends ActivityBase {
             @Override
             public void onItemClick(AdapterView<?> arg0, View view, int position, long id)
             {
-                HashMap<Object, String> map = mVideoArray.get(position);
-                String url = map.get("link");
+                HashMap<String,Object> map = mVideoArray.get(position);
+                String url = (String)map.get("link");
                 try{
                     videoView1.setVideoURI(Uri.parse(url));
                     boolean blFocus = videoView1.requestFocus();
@@ -91,23 +90,23 @@ public class Videotest1Activity extends ActivityBase {
     }
     
     private void getVideoList(){
-        HashMap<Object, String> map = null;
+        HashMap<String,Object> map = null;
         
         //wenjing road
-        map = new HashMap<Object, String>();
+        map = new HashMap<String,Object>();
         map.put("road","文景路");
         map.put("link", rtspUrlWenjinglu);
         mVideoArray.add(map);
         
         //zhonglou tower
-        map = new HashMap<Object, String>();
+        map = new HashMap<String,Object>();
         map.put("road","钟楼");
         map.put("link", rtspUrlZhonglu);
         mVideoArray.add(map);
         
         //taihua road
       //zhonglou tower
-        map = new HashMap<Object, String>();
+        map = new HashMap<String,Object>();
         map.put("road","太华路");
         map.put("link", rtspUrlTaihualu);
         mVideoArray.add(map);
